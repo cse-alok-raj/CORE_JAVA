de600826d87642638286fffcab23fc1ab137836c
cse-alok-raj <cse.alok.raj@gmail.com>
2026-01-18 15:27:36 +0530

    Daily Updates

---
+++ module-3/Array/practice/JavaApplication.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.capgemini.module_3_array.practice;
+
+import java.io.StreamTokenizer;
+
+/**
+ * 
+ */
+public interface JavaApplication {
+	public static void execution() {
+		//EvenOdd.display();
+		//StoreDisplay.digits();
+//		StoreDisplay.upperCase();
+//		StoreDisplay.lowerCase();
+//		StoreDisplay.weekDays();
+//		StoreDisplay.monthYear();
+//		StoreDisplay.directions();
+		StudentObject.stud();
+	}
+}
